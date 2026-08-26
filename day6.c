0a5a2416d25b2660b8245f1f77e0e3474a7f4c70
Anika Goyal <306167992+anikagoyal336@users.noreply.github.com>
2026-08-26 19:52:21 +0530

    Add day 6 switch program

---
+++ day6.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+int a, b, c, d, max;
+printf("\n Enter four numbers:");
+scanf("%d %d %d %d", &a, &b, &c, &d);
+if (a>b) 
+{
+    if (a>c) 
+    {
+        if (a>d) 
+        {
+            max=a;
+        }
+        else 
+        {
+            max=d;
+        }
+    }
+    else 
+    {
+        if (c>d) 
+        {
+            max=c;
+        }
+        else 
+        {
+            max=d;
+        }
+    }
+}
+else 
+{
+    if (b>c) 
+    {
+        if (b>d) 
+        {
+            max=b;
+        }
+        else 
+        {
+            max=d;
+        }
+    }
+    else 
+    {
+        if (c>d) 
+        {
+            max=c;
+        }
+        else 
+        {
+            max=d;
+        }
+    }
+}
+printf("\n Maximum number is: %d", max);
+return 0;
+}
